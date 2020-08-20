60e19d12ae2b078dc4dce4cbfca5391cc772e8f2
bbonenfant <bonenfan5ben@gmail.com>
2020-08-20 16:03:16 -0400

    Creating python package testing workflow. (#4)
    * Creating python package testing workflow.
    
    This .github/workflows/test.yml configuration file is an adaption of the pendulum test.yml workflow file. (link: https://github.com/sdispater/pendulum/blob/master/.github/workflows/tests.yml)
    
    This workflow performs the following steps:
    - Run listing using the pre-commit package. If this step fails the tests do not run.
    - Create the testing environment with poetry.
    - Run the tests for the repository.
    - Builds and installs the package.
    
    * Revamped the testing infrastructure for the test github workflow.
    
    Restructured the testing configuration to put everything in the "tests" directory and put all python files of tests within the "test_snappiershot" directory.
    
    - Created "mypy.ini" and "pytest.ini" files.
    - Moved the ".coveragerc" and ".flake8" files to the "tests" directory.
    - Moved the files that were previously in the "tests" directory down into the "tests/test_snappiershot" directory.
    - Updated the ".pre-commit-config.yaml" file to run the mypy static type checking.
    - Removed the "pytest-mypy" plugin as a testing dependency, as this check will be done by the "pre-commit" plugin.
    
    The pre-commit jobs can be run with the command: pre-commit run --all-files
    The tests can be run with the command: poetry run pytest tests
    
    Co-authored-by: Ben Bonenfant <bbonenfant@morsecorp.com>

--- snappiershot/__init__.py
+++ snappiershot/__init__.py
@@ -6,11 +6,11 @@ try:
     import importlib.metadata as metadata
 except ImportError:
     #  < 3.8
-    import importlib_metadata as metadata
+    import importlib_metadata as metadata  # type: ignore
 
 
 __version__ = ""
 try:
-    __version__ = metadata.version(__name__)
+    __version__ = metadata.version(__name__)  # type: ignore
 except FileNotFoundError:
     pass
