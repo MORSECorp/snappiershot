cf116aa4300e7b715776414358f93ec1dc26e9b7
bbonenfant <bonenfan5ben@gmail.com>
2020-08-27 20:35:11 -0400

    Snappiershot Configuration (#11)
    Implemented the Config object and parsing of configurations from the `pyproject.toml` file.
    
    * The tomlkit package was added as a dependency. This package has no
      dependencies for python3.
    * Configuration through the `pyproject.toml` file is implemented under
      the `[tool.snappiershot]` header. The following configurations are
      currently supported:
      * file_format (this name was chosen to avoid namespace clashes with the format function)
      * significant_figures
      * absolute_tolerance
      * relative_tolerance
    * The `README.md` file was updated with information about configuration
      through the `pyproject.toml` file.
    
    Co-authored-by: Ben Bonenfant <bbonenfant@morsecorp.com>

---
+++ snappiershot/config.py
@@ -0,0 +1,147 @@
+""" Functionality relating to configuration of the Snappiershot package. """
+from pathlib import Path
+from typing import Any, Dict, Optional, Union
+
+import tomlkit
+from tomlkit.exceptions import TOMLKitError
+
+PathType = Union[Path, str]
+
+DEFAULT_FILE_FORMAT = "JSON"
+DEFAULT_SIG_FIG = 6
+DEFAULT_ABS_TOL = 1e-6
+DEFAULT_REL_TOL = 0.001
+
+
+class Config:
+    """ The configuration object used throughout the package. """
+
+    def __init__(
+        self,
+        file_format: str = DEFAULT_FILE_FORMAT,
+        significant_figures: int = DEFAULT_SIG_FIG,
+        float_absolute_tolerance: float = DEFAULT_ABS_TOL,
+        float_relative_tolerance: float = DEFAULT_REL_TOL,
+        **_kwargs: Dict,
+    ):
+        """
+        Args:
+            file_format: The file format type to use, current options are {JSON}.
+            significant_figures: The number of significant figures to use by default.
+            float_absolute_tolerance: The absolute tolerance used to compare floats.
+            float_relative_tolerance: The relative tolerance used to compare floats.
+
+        The ``**_kwargs`` argument is used to silently accept and ignore any extraneous
+          configuration key-value pairs that are parsed from the pyproject.toml file.
+        """
+        self.file_format = file_format
+        self.sig_fig = significant_figures
+        self.abs_tol = float_absolute_tolerance
+        self.rel_tol = float_relative_tolerance
+        self._validate()
+
+    @classmethod
+    def from_pyproject(cls, file: Path) -> "Config":
+        """ Create a Config object by parsing a pyproject.toml file.
+
+        Args:
+            file: The path to the pyproject.toml file, as a `pathlib.Path` object.
+        """
+        try:
+            pyproject_toml = tomlkit.parse(file.read_text())
+            tool_config = pyproject_toml.get("tool", dict()).get("snappiershot", dict())
+        except (TOMLKitError, FileNotFoundError):
+            tool_config = dict()
+        return Config(**tool_config)
+
+    def _validate(self) -> None:
+        """ Validates the configuration.
+
+        Raises:
+            ValueError: If a configuration has an invalid value.
+            TypeError: If a configuration has an invalid type.
+        """
+        # Validate file_format.
+        if not isinstance(self.file_format, str):
+            raise TypeError(
+                f"Expected a string value for the file_format configuration; "
+                f"Found: {self.file_format}"
+            )
+        self.file_format = str(self.file_format).upper()  # Convert from tomlkit type.
+        if self.file_format != DEFAULT_FILE_FORMAT:
+            raise ValueError(
+                f"The only supported file_format is {DEFAULT_FILE_FORMAT}; "
+                f"Found: {self.file_format}"
+            )
+
+        # Validate sig_fig.
+        if not isinstance(self.sig_fig, int):
+            raise TypeError(
+                f"Expected an integer value for the significant_figures configuration; "
+                f"Found: {self.sig_fig}"
+            )
+        self.sig_fig = int(self.sig_fig)  # Convert from tomlkit type.
+        if self.sig_fig <= 0:
+            raise ValueError(
+                f"The significant_figures configuration must be a positive integer; "
+                f"Found: {self.sig_fig}"
+            )
+
+        # Validate abs_tol.
+        if not isinstance(self.abs_tol, float):
+            raise TypeError(
+                f"Expected a float value for the float_absolute_tolerance configuration; "
+                f"Found: {self.abs_tol}"
+            )
+        self.abs_tol = float(self.abs_tol)  # Convert from tomlkit type.
+        if self.abs_tol < 0:
+            raise ValueError(
+                f"The float_absolute_tolerance configuration must be positive; "
+                f"Found: {self.abs_tol}"
+            )
+
+        # Validate rel_tol.
+        if not isinstance(self.rel_tol, float):
+            raise TypeError(
+                f"Expected a float value for the float_relative_tolerance configuration; "
+                f"Found: {self.rel_tol}"
+            )
+        self.rel_tol = float(self.rel_tol)  # Convert from tomlkit type.
+        if self.rel_tol < 0:
+            raise ValueError(
+                f"The float_relative_tolerance configuration must be positive; "
+                f"Found: {self.rel_tol}"
+            )
+
+    def __eq__(self, other: Any) -> bool:
+        """ Check equality. """
+        return isinstance(other, type(self)) and (vars(self) == vars(other))
+
+    def __repr__(self) -> str:
+        """ Human-readable representation. """
+        return (
+            "Config("
+            f"file_format={self.file_format}, "
+            f"sig_fig={self.sig_fig}, "
+            f"abs_tol={self.abs_tol}, "
+            f"rel_tol={self.rel_tol})"
+        )
+
+
+def find_pyproject_toml(source: PathType) -> Optional[Path]:
+    """ Travel up the tree of the provided source, looking for a pyproject.toml file.
+
+    Args:
+        source: A path to the source from which to start the search.
+
+    Returns:
+        Path to the pyproject.toml file, if found, else None.
+    """
+    source = Path(source)
+    haystack = [source, *source.parents] if source.is_dir() else source.parents
+    for directory in haystack:
+        needle = directory / "pyproject.toml"
+        if needle.exists():
+            return needle
+    else:
+        return None
---
+++ tests/test_config.py
@@ -0,0 +1,110 @@
+""" Tests for snappiershot/config.py """
+from pathlib import Path
+from typing import Dict, List, Type
+
+import pytest
+from snappiershot.config import (
+    DEFAULT_ABS_TOL,
+    DEFAULT_FILE_FORMAT,
+    DEFAULT_REL_TOL,
+    DEFAULT_SIG_FIG,
+    Config,
+    find_pyproject_toml,
+)
+
+DEFAULT_CONFIG_KWARGS = dict(
+    file_format=DEFAULT_FILE_FORMAT,
+    significant_figures=DEFAULT_SIG_FIG,
+    float_absolute_tolerance=DEFAULT_ABS_TOL,
+    float_relative_tolerace=DEFAULT_REL_TOL,
+)
+
+
+# ===== Fixtures ===========================================
+
+
+@pytest.fixture(name="pyproject_directory")
+def _pyproject_directory(tmp_path: Path) -> Path:
+    """ Fixture which creates the following directory structure in a temporary directory:
+
+    temp
+      | - child
+            | - pyproject.toml
+            | - grandchild
+                  | - great-grandchild
+
+    Returns:
+        The lowest directory within the created directory structure.
+    """
+    lowest_directory = tmp_path / "child" / "grandchild" / "great-grandchild"
+    lowest_directory.mkdir(parents=True)
+    (tmp_path / "child" / "pyproject.toml").touch()
+    return lowest_directory
+
+
+# ===== Tests ==============================================
+
+
+@pytest.mark.parametrize("parent_index, expected", [(0, True), (1, True), (2, False)])
+def test_find_pyproject_toml(parent_index: int, expected: bool, pyproject_directory: Path):
+    """ Test that the `snappiershot.config.find_pyproject_toml` function properly
+    traverses the provided path searching for the pyproject.toml file.
+    """
+    # Arrange
+    source = pyproject_directory.parents[parent_index]
+
+    # Act
+    pyproject_toml = find_pyproject_toml(source)
+
+    # Assert
+    assert (pyproject_toml is not None) is expected
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    "contents, config_kwargs",
+    [
+        ([], DEFAULT_CONFIG_KWARGS),
+        (["[tool.snappiershot]"], DEFAULT_CONFIG_KWARGS),
+        (["[tool.snappiershot]", "bad line"], DEFAULT_CONFIG_KWARGS),
+        (["[tool.snappiershot]", "ignored_config = true"], DEFAULT_CONFIG_KWARGS),
+        (["[tool.snappiershot]", "significant_figures = 2"], {**DEFAULT_CONFIG_KWARGS, "significant_figures": 2}),
+        (["[tool.snappiershot]", "float_absolute_tolerance = 1E-4"], {**DEFAULT_CONFIG_KWARGS, "float_absolute_tolerance": 1e-4}),
+        (["[tool.snappiershot]", "float_relative_tolerance = 0.1"], {**DEFAULT_CONFIG_KWARGS, "float_relative_tolerance": 0.1}),
+    ]
+)
+# fmt: on
+def test_from_pyproject(contents: List[str], config_kwargs: Dict, pyproject_directory: Path):
+    """ Test that pyproject.toml files are parsed for configurations in a robust manner. """
+    # Arrange
+    pyproject_toml = find_pyproject_toml(pyproject_directory)
+    pyproject_toml.open("w").writelines((f"{line}\n" for line in contents))
+    expected = Config(**config_kwargs)
+
+    # Act
+    result = Config.from_pyproject(pyproject_toml)
+
+    # Assert
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "config_kwargs, expected_error",
+    [
+        ({**DEFAULT_CONFIG_KWARGS, "file_format": None}, TypeError),
+        ({**DEFAULT_CONFIG_KWARGS, "file_format": "csv"}, ValueError),
+        ({**DEFAULT_CONFIG_KWARGS, "significant_figures": 3.14}, TypeError),
+        ({**DEFAULT_CONFIG_KWARGS, "significant_figures": -1}, ValueError),
+        ({**DEFAULT_CONFIG_KWARGS, "float_absolute_tolerance": 2}, TypeError),
+        ({**DEFAULT_CONFIG_KWARGS, "float_absolute_tolerance": -1e6}, ValueError),
+        ({**DEFAULT_CONFIG_KWARGS, "float_relative_tolerance": 2}, TypeError),
+        ({**DEFAULT_CONFIG_KWARGS, "float_relative_tolerance": -1e6}, ValueError),
+    ]
+)
+def test_config_validate(config_kwargs: Dict, expected_error: Type[Exception]):
+    """ Checks that validation of the configuration values occurs as expected. """
+    # Arrange
+
+    # Act & Assert
+    with pytest.raises(expected_error):
+        Config(**config_kwargs)
