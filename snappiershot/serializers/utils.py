3cf2741f818f1aaf7d3009c269bcccdcf73851b7
Ben Bonenfant <bbonenfant@morsecorp.com>
2020-09-14 14:59:28 -0400

    Updated with PR comments.

--- snappiershot/serializers/utils.py
+++ snappiershot/serializers/utils.py
@@ -14,6 +14,17 @@ def get_snapshot_file(test_file: Path, suffix: str) -> Path:
         suffix: The file extension for the snapshot file.
           Should include the dot, e.g. ".json"
     """
+    # Error checking.
+    if not test_file.parent.exists():
+        raise NotADirectoryError(
+            f"The directory containing the test file does not exist: {test_file.parent}"
+        )
+    if not suffix.startswith("."):
+        raise TypeError(
+            'Suffix is not a valid file extension; it must start with a "dot", i.e. ".json"'
+            f" -- Found: {suffix}"
+        )
+
     snapshot_directory = test_file.parent.joinpath(SNAPSHOT_DIRECTORY)
     snapshot_directory.mkdir(exist_ok=True)
     return snapshot_directory.joinpath(test_file.name).with_suffix(suffix)
--- tests/test_serializers/test_utils.py
+++ tests/test_serializers/test_utils.py
@@ -1,4 +1,5 @@
 """ Tests for snappiershot/serializers/utils.py """
+import pytest
 from snappiershot.serializers.utils import get_snapshot_file
 
 
@@ -15,3 +16,27 @@ def test_get_snapshot_file(tmp_path):
     # Assert
     assert returned == expected
     assert returned.parent.exists()
+
+
+def test_get_snapshot_file_directory_error(tmp_path):
+    """ Test that get_snapshot_file raises an error if the directory
+    containing the test does not exist.
+    """
+    # Arrange
+    test_file = tmp_path / "sub-directory" / "example_test.py"
+    suffix = ".json"
+
+    # Act & Assert
+    with pytest.raises(NotADirectoryError):
+        get_snapshot_file(test_file, suffix)
+
+
+def test_get_snapshot_file_type_error(tmp_path):
+    """ Test that get_snapshot_file raises an error for an invalid suffix. """
+    # Arrange
+    test_file = tmp_path / "example_test.py"
+    suffix = "json"
+
+    # Act & Assert
+    with pytest.raises(TypeError):
+        get_snapshot_file(test_file, suffix)
