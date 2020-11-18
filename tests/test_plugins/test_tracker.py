335a0e7f31680c4aeb298ce485b28234814b31f0
Ben Bonenfant <bonenfan5ben@gmail.com>
2020-11-18 11:46:40 -0500

    AssertionError Reporting (#62)
    * Created the human-readable differ for AssertionErrors
    * Added pprint_ordered_sets package as dependency.
    * Used combination of pprint and difflib standard library modules
    to construct the human readable diffs.
    * Fixed bug where collections (tuples and sets) were not abled to be
    asserted.
    * Added SnappierShot summary to the pytest output.

--- tests/test_plugins/test_tracker.py
+++ tests/test_plugins/test_tracker.py
@@ -1,8 +1,56 @@
 """ Tests for snappiershot/plugins/tracker.py """
 from pathlib import Path
 
-from snappiershot.plugins.tracker import SnapshotTracker
+from snappiershot.plugins.tracker import SnapshotTracker, StatusReport
 from snappiershot.snapshot import CallerInfo, SnapshotMetadata, SnapshotStatus
+
+
+def test_tracker_get_status_report():
+    """ Test that the SnapshotTracker.get_status_report method functions as expected. """
+    # Arrange
+    expected = StatusReport(1, 2, 3, 4, 5)
+
+    tracker = SnapshotTracker()
+    snapshot_file = Path(__file__)
+    function_name = "test_function"
+    metadata = SnapshotMetadata(
+        caller_info=CallerInfo(snapshot_file, function_name, dict()),
+        update_on_next_run=False,
+        test_runner_provided_name="",
+        user_provided_name="",
+    )
+    tracker.snapshots = {
+        snapshot_file: {
+            function_name: [
+                dict(
+                    metadata=metadata.as_dict(),
+                    snapshots=[SnapshotStatus.UNCHECKED] * expected.unchecked,
+                ),
+                dict(
+                    metadata=metadata.as_dict(),
+                    snapshots=[SnapshotStatus.FAILED] * expected.failed,
+                ),
+                dict(
+                    metadata=metadata.as_dict(),
+                    snapshots=[SnapshotStatus.PASSED] * expected.passed,
+                ),
+                dict(
+                    metadata=metadata.as_dict(),
+                    snapshots=[SnapshotStatus.RECORDED] * expected.recorded,
+                ),
+                dict(
+                    metadata=metadata.as_dict(),
+                    snapshots=[SnapshotStatus.WRITTEN] * expected.written,
+                ),
+            ]
+        }
+    }
+
+    # Act
+    status_report = tracker.get_status_report()
+
+    # Assert
+    assert status_report == expected
 
 
 def test_tracker_set_status():
--- tests/test_serializers/test_utils.py
+++ tests/test_serializers/test_utils.py
@@ -74,7 +74,7 @@ class TestDefaultEncodeValue:
 
     @staticmethod
     def test_encode_serializable_types():
-        """ Test that a class with custom-encoded types dose not encode these types. """
+        """ Test that a class with custom-encoded types does not encode these types. """
         # Arrange
         from datetime import datetime
 
@@ -88,6 +88,18 @@ class TestDefaultEncodeValue:
 
         # Assert
         assert result == expected
+
+    @staticmethod
+    @pytest.mark.parametrize("value", (set("abcdefg"), (1, 2, 3)))
+    def test_encode_collection_types(value):
+        """ Test that a class with custom-encoded types does not encode collection. """
+        # Arrange
+
+        # Act
+        result = default_encode_value(value)
+
+        # Assert
+        assert result == value
 
     @staticmethod
     @pytest.mark.parametrize("value", [isinstance, type, iter([1, 2])])
