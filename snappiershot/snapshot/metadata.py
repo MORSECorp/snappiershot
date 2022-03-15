f34e28364bb0ec07004a6d4b1d8ce7e5353af0e7
Tahoe Schrader <tschrader@morsecorp.com>
2022-03-15 12:13:56 -0400

    Improve metadata matching to cooperate with complex classes

--- snappiershot/snapshot/metadata.py
+++ snappiershot/snapshot/metadata.py
@@ -43,7 +43,47 @@ class SnapshotMetadata:
         Args:
             metadata_dict: The "metadata" section to compare against this object.
         """
-        return self.caller_info.args == metadata_dict["arguments"]
+        # If this dictionary can instantiate into a class, do it.
+        metadata_args = self.caller_info.args
+        dict_to_compare: dict = {}
+        for key in metadata_args.keys():
+            dict_to_compare[key] = None
+            metadata_obj = metadata_args.get(key)
+            dict_from_file = metadata_dict["arguments"].get(key)
+
+            metadata_obj_has_method = hasattr(metadata_obj, "from_dict")
+            metadata_obj_is_not_none = metadata_obj is not None
+            dict_from_file_is_not_none = dict_from_file is not None
+
+            if isinstance(metadata_obj, list) and isinstance(dict_from_file, list):
+                dict_to_compare[key] = list()
+                for kk in range(len(metadata_obj)):
+                    ii = metadata_obj[kk]
+                    jj = dict_from_file[kk]
+
+                    metadata_obj_has_method = hasattr(ii, "from_dict")
+                    metadata_obj_is_not_none = ii is not None
+                    dict_from_file_is_not_none = jj is not None
+
+                    if type(ii) == type(jj):
+                        dict_to_compare[key].append(jj)
+                    elif (
+                        metadata_obj_has_method
+                        and metadata_obj_is_not_none
+                        and dict_from_file_is_not_none
+                    ):
+                        dict_to_compare[key].append(ii.from_dict(jj))
+            else:
+                if type(metadata_obj) == type(dict_from_file):
+                    dict_to_compare[key] = dict_from_file
+                elif (
+                    metadata_obj_has_method
+                    and metadata_obj_is_not_none
+                    and dict_from_file_is_not_none
+                ):
+                    dict_to_compare[key] = metadata_obj.from_dict(dict_from_file)
+
+        return metadata_args == dict_to_compare
 
     def _validate(self) -> None:
         """ Validates the snapshot metadata.
