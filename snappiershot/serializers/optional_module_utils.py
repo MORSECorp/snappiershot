70bd2505068453e13bf13398b12981bf5193bd82
Tahoe Schrader <tschrader@morsecorp.com>
2022-03-16 18:51:02 -0400

    Allow for encoding to be skipped for random number generators, fixed

--- snappiershot/serializers/optional_module_utils.py
+++ snappiershot/serializers/optional_module_utils.py
@@ -178,7 +178,7 @@ class Numpy:
         if isinstance(value, cls._get_numpy_primatives(np)):
             return value.item()  # type: ignore
 
-        if isinstance(value, np.random._generator.Generator):  # type: ignore
+        if isinstance(value, np.random.Generator):  # type: ignore
             return "numpy.random._generator.Generator - encoding skipped"
 
         raise NotImplementedError(
